c067c8178b8f2889851122259124646048dcb6b7
mu-fumi <1017182993@qq.com>
2019-05-24 00:18:15 +0800

    n

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Icon, Button } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 
 import { BrowserRouter, Route, Switch, exact, Redirect, Link } from 'react-router-dom';
 import { getMenuLists } from "./api.js";
@@ -9,20 +9,18 @@ import { routes } from '@/pages/Layout/routers';
 
 import './index.less';
 
-const { Header, Sider, Content, } = Layout;
+const { Header, Sider, Content } = Layout;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 export default class Index extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            leftList: [],
-            openKeys: ['sub1'],
-            collapsed: false,
-        }
-    }
+
+    state = {
+        collapsed: false,
+        openKeys: ['sub1'],
+    };
+
 
     toggle = () => {
         this.setState({
@@ -30,7 +28,8 @@ export default class Index extends Component {
         });
     };
 
-    rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+
+
 
     //WARNING! To be deprecated in React v17. Use componentDidMount instead.
     async componentWillMount() {
@@ -44,31 +43,20 @@ export default class Index extends Component {
     }
 
 
-    changeLeftPos() {
-        const { leftWidth } = this.state.indexTop;
-        const width = leftWidth === 200 ? 0 : 200;
-        this.setState({
-            indexTop: {
-                leftWidth: width
-            }
-        })
-    }
+    rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
 
 
 
     onOpenChange = openKeys => {
         const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
         if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-          this.setState({ openKeys });
+            this.setState({ openKeys });
         } else {
-          this.setState({
-            openKeys: latestOpenKey ? [latestOpenKey] : [],
-          });
+            this.setState({
+                openKeys: latestOpenKey ? [latestOpenKey] : [],
+            });
         }
-      };
-    
-
-
+    };
 
 
 
@@ -76,40 +64,38 @@ export default class Index extends Component {
         const { collapsed } = this.state;
 
         return (
+
+
+
             <Layout>
                 <Header style={{ background: '#fff', padding: 0 }}>
-                    <div className="logo" >
-
-                    </div>
                     <Icon
                         className="trigger"
                         type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
                         onClick={this.toggle}
                     />
-
                 </Header>
-                <Layout>
-                    <Sider trigger={null} 
-                        collapsible 
-                        collapsed={this.state.collapsed} 
-                        onCollapse={this.onCollapse} >
 
-                        <Menu
-                            mode="inline" 
-                            defaultSelectedKeys={['1']}
+                <Layout>
+                    <Sider trigger={null}
+                        collapsible
+                        theme="light"
+                        collapsed={this.state.collapsed}
+                        style={{
+                            overflow: 'auto',
+                            height: '100vh',
+                            position: 'fixed',
+                            left: 0,
+                        }}
+                    >
+                        <Menu theme="light"
+                            mode="inline"
+                            openKeys={this.state.openKeys}
+                            defaultOpenKeys={this.state.openKeys}
+                            onOpenChange={this.onOpenChange}
                         >
-                            <Menu.Item key="1">
-                                <Icon type="pie-chart" />
-                                <span>Option 1</span>
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                                <Icon type="desktop" />
-                                <span>Option 2</span>
-                            </Menu.Item>
-                            <Menu.Item key="3">
-                                <Icon type="inbox" />
-                                <span>Option 3</span>
-                            </Menu.Item>
+
+
                             <SubMenu
                                 key="sub1"
                                 title={
@@ -119,10 +105,10 @@ export default class Index extends Component {
                                     </span>
                                 }
                             >
-                                <Menu.Item key="5">Option 5</Menu.Item>
-                                <Menu.Item key="6">Option 6</Menu.Item>
-                                <Menu.Item key="7">Option 7</Menu.Item>
-                                <Menu.Item key="8">Option 8</Menu.Item>
+                                <Menu.Item key="1">Option 1</Menu.Item>
+                                <Menu.Item key="2">Option 2</Menu.Item>
+                                <Menu.Item key="3">Option 3</Menu.Item>
+                                <Menu.Item key="4">Option 4</Menu.Item>
                             </SubMenu>
                             <SubMenu
                                 key="sub2"
@@ -133,25 +119,80 @@ export default class Index extends Component {
                                     </span>
                                 }
                             >
-                                <Menu.Item key="9">Option 9</Menu.Item>
-                                <Menu.Item key="10">Option 10</Menu.Item>
+                                <Menu.Item key="5">Option 5</Menu.Item>
+                                <Menu.Item key="6">Option 6</Menu.Item>
                                 <SubMenu key="sub3" title="Submenu">
-                                    <Menu.Item key="11">Option 11</Menu.Item>
-                                    <Menu.Item key="12">Option 12</Menu.Item>
+                                    <Menu.Item key="7">Option 7</Menu.Item>
+                                    <Menu.Item key="8">Option 8</Menu.Item>
                                 </SubMenu>
                             </SubMenu>
-                        </Menu>
+                            <SubMenu
+                                key="sub4"
+                                title={
+                                    <span>
+                                        <Icon type="setting" />
+                                        <span>Navigation Three</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="9">Option 9</Menu.Item>
+                                <Menu.Item key="10">Option 10</Menu.Item>
+                                <Menu.Item key="11">Option 11</Menu.Item>
+                                <Menu.Item key="12">Option 12</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub5"
+                                title={
+                                    <span>
+                                        <Icon type="setting" />
+                                        <span>Navigation Three</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="9">Option 9</Menu.Item>
+                                <Menu.Item key="10">Option 10</Menu.Item>
+                                <Menu.Item key="11">Option 11</Menu.Item>
+                                <Menu.Item key="12">Option 12</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub6"
+                                title={
+                                    <span>
+                                        <Icon type="setting" />
+                                        <span>Navigation Three</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="9">Option 9</Menu.Item>
+                                <Menu.Item key="10">Option 10</Menu.Item>
+                                <Menu.Item key="11">Option 11</Menu.Item>
+                                <Menu.Item key="12">Option 12</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub7"
+                                title={
+                                    <span>
+                                        <Icon type="setting" />
+                                        <span>Navigation Three</span>
+                                    </span>
+                                }
+                            >
+                                <Menu.Item key="9">Option 9</Menu.Item>
+                                <Menu.Item key="10">Option 10</Menu.Item>
+                                <Menu.Item key="11">Option 11</Menu.Item>
+                                <Menu.Item key="12">Option 12</Menu.Item>
+                            </SubMenu>
 
+
+                        </Menu>
                     </Sider>
-                    <Content>Content</Content>
+                    <Content
+                        style={{ margin: '24px 16px 0', marginLeft: 200, overflow: 'initial' }}
+                    >
+                        Content
+              </Content>
                 </Layout>
             </Layout>
-
-
-
-
-
-
 
 
 
