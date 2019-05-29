36d999ceec1cc823f331f570e93d8756db738d05
许豪 <1017182993@qq.com>
2019-05-29 18:10:29 +0800

    ;io;

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -9,7 +9,7 @@ import { routes } from '@/pages/Layout/routers';
 
 import './index.less';
 
-const { Header, Sider, Content } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
@@ -65,138 +65,180 @@ export default class Index extends Component {
 
         return (
 
-
-
             <Layout>
-                <Header style={{ background: '#fff', padding: 0 }}>
-                    <Icon
-                        className="trigger"
-                        type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                        onClick={this.toggle}
-                    />
-                </Header>
 
-                <Layout>
-                    <Sider trigger={null}
-                        collapsible
-                        theme="light"
-                        collapsed={this.state.collapsed}
-                        style={{
-                            overflow: 'auto',
-                            height: '100vh',
-                            position: 'fixed',
-                            left: 0,
-                        }}
-                    >
-                        <Menu theme="light"
-                            mode="inline"
-                            openKeys={this.state.openKeys}
-                            defaultOpenKeys={this.state.openKeys}
-                            onOpenChange={this.onOpenChange}
+               
+               
+               
+                <Header style={{ position: 'fixed', zIndex: 1, width: '100%',background:'#fff' }} />
+
+
+                <Sider
+                    style={{
+                        overflow: 'auto',
+                        height: '100vh',
+                        position: 'fixed',
+                        background:'#fff',
+                        left: 0,
+                        top: 65,
+                    }}
+                >
+                    <Menu theme="" mode="inline" 
+                        defaultSelectedKeys={['4']}
+                        onOpenChange={this.onOpenChange}
+                        openKeys={this.state.openKeys}
                         >
+                        <SubMenu
+                            key="sub1"
+                            title={
+                                <span>
+                                    <Icon type="mail" />
+                                    <span>Navigation One</span>
+                                </span>
+                            }
+                        >
+                            <Menu.Item key="1">Option 1</Menu.Item>
+                            <Menu.Item key="2">Option 2</Menu.Item>
+                            <Menu.Item key="3">Option 3</Menu.Item>
+                            <Menu.Item key="4">Option 4</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub2"
+                            title={
+                                <span>
+                                    <Icon type="appstore" />
+                                    <span>Navigation Two</span>
+                                </span>
+                            }
+                        >
+                            <Menu.Item key="5">Option 5</Menu.Item>
+                            <Menu.Item key="6">Option 6</Menu.Item>
+                            <SubMenu key="sub3" title="Submenu">
+                                <Menu.Item key="7">Option 7</Menu.Item>
+                                <Menu.Item key="8">Option 8</Menu.Item>
+                            </SubMenu>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub4"
+                            title={
+                                <span>
+                                    <Icon type="setting" />
+                                    <span>Navigation Three</span>
+                                </span>
+                            }
+                        >
+                            <Menu.Item key="9">Option 9</Menu.Item>
+                            <Menu.Item key="10">Option 10</Menu.Item>
+                            <Menu.Item key="11">Option 11</Menu.Item>
+                            <Menu.Item key="12">Option 12</Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
 
+                <Layout style={{ marginLeft: 200 }}>
 
-                            <SubMenu
-                                key="sub1"
-                                title={
-                                    <span>
-                                        <Icon type="mail" />
-                                        <span>Navigation One</span>
-                                    </span>
-                                }
-                            >
-                                <Menu.Item key="1">Option 1</Menu.Item>
-                                <Menu.Item key="2">Option 2</Menu.Item>
-                                <Menu.Item key="3">Option 3</Menu.Item>
-                                <Menu.Item key="4">Option 4</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub2"
-                                title={
-                                    <span>
-                                        <Icon type="appstore" />
-                                        <span>Navigation Two</span>
-                                    </span>
-                                }
-                            >
-                                <Menu.Item key="5">Option 5</Menu.Item>
-                                <Menu.Item key="6">Option 6</Menu.Item>
-                                <SubMenu key="sub3" title="Submenu">
-                                    <Menu.Item key="7">Option 7</Menu.Item>
-                                    <Menu.Item key="8">Option 8</Menu.Item>
-                                </SubMenu>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub4"
-                                title={
-                                    <span>
-                                        <Icon type="setting" />
-                                        <span>Navigation Three</span>
-                                    </span>
-                                }
-                            >
-                                <Menu.Item key="9">Option 9</Menu.Item>
-                                <Menu.Item key="10">Option 10</Menu.Item>
-                                <Menu.Item key="11">Option 11</Menu.Item>
-                                <Menu.Item key="12">Option 12</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub5"
-                                title={
-                                    <span>
-                                        <Icon type="setting" />
-                                        <span>Navigation Three</span>
-                                    </span>
-                                }
-                            >
-                                <Menu.Item key="9">Option 9</Menu.Item>
-                                <Menu.Item key="10">Option 10</Menu.Item>
-                                <Menu.Item key="11">Option 11</Menu.Item>
-                                <Menu.Item key="12">Option 12</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub6"
-                                title={
-                                    <span>
-                                        <Icon type="setting" />
-                                        <span>Navigation Three</span>
-                                    </span>
-                                }
-                            >
-                                <Menu.Item key="9">Option 9</Menu.Item>
-                                <Menu.Item key="10">Option 10</Menu.Item>
-                                <Menu.Item key="11">Option 11</Menu.Item>
-                                <Menu.Item key="12">Option 12</Menu.Item>
-                            </SubMenu>
-                            <SubMenu
-                                key="sub7"
-                                title={
-                                    <span>
-                                        <Icon type="setting" />
-                                        <span>Navigation Three</span>
-                                    </span>
-                                }
-                            >
-                                <Menu.Item key="9">Option 9</Menu.Item>
-                                <Menu.Item key="10">Option 10</Menu.Item>
-                                <Menu.Item key="11">Option 11</Menu.Item>
-                                <Menu.Item key="12">Option 12</Menu.Item>
-                            </SubMenu>
-
-
-                        </Menu>
-                    </Sider>
-                    <Content
-                        style={{ margin: '24px 16px 0', marginLeft: 200, overflow: 'initial' }}
-                    >
-                        Content
-              </Content>
+                    <Content style={{ margin: '65px 0 0 16px', overflow: 'initial' }}>
+                        <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                            ...
+          <br />
+                            Really
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            long
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            ...
+          <br />
+                            content
+        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
                 </Layout>
             </Layout>
-
-
-
-
         )
     }
 }
